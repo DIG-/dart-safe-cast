2a85d31498572c8869f91d32ff1124d9f56aad3b
Gustavo de Oliveira Silva (DIG) <contato@dig.dev.br>
2024-07-20 12:26:59 -0300

    Created tests for Cast instance

--- test/cast_instance_test.dart
+++ test/cast_instance_test.dart
@@ -5,92 +5,92 @@ void main() {
   final String sample = 'sample';
   final String empty = '';
   final Map<String, String> map = {sample: empty};
-  group('Test for Cast static', () {
-    test('Cast.as<String>(\'\')', () {
+  group('Test for Cast instance', () {
+    test('Cast(\'\').to<String>()', () {
       expect(empty, isNotNull);
-      final value = Cast.as<String>(empty);
+      final value = Cast(empty).to<String>();
       expect(value, equals(empty));
     });
-    test('Cast.as<String>(\'$sample\')', () {
+    test('Cast(\'$sample\').to<String>()', () {
       expect(sample, isNotNull);
-      final value = Cast.as<String>(sample);
+      final value = Cast(sample).to<String>();
       expect(value, equals(sample));
       expect(value, isNot(empty));
     });
-    test('Cast.as<String>($map)', () {
+    test('Cast($map).to<String>()', () {
       expect(map, isNotNull);
       expect(map, isMap);
       try {
-        Cast.as<String>(map);
+        Cast(map).to<String>();
       } catch (e) {
         expect(e, isA<TypeError>());
         return;
       }
       fail('Must throw exception');
     });
-    test('Cast.as<Map>($map)', () {
+    test('Cast($map).to<Map>()', () {
       expect(map, isNotNull);
       expect(map, isMap);
-      final value = Cast.as<Map>(map);
+      final value = Cast(map).to<Map>();
       expect(value, isNotNull);
       expect(value, isMap);
       expect(value, equals(map));
     });
-    test('Cast.as<Map<int, String>>($map)', () {
+    test('Cast($map).to<Map<int, String>>()', () {
       expect(map, isNotNull);
       expect(map, isMap);
       try {
-        Cast.as<Map<int, String>>(map);
+        Cast(map).to<Map<int, String>>();
       } catch (e) {
         expect(e, isA<TypeError>());
         return;
       }
       fail('Must throw exception');
     });
-    test('Cast.as<String>(null)', () {
+    test('Cast(null).to<String>()', () {
       try {
-        Cast.as<String>(null);
+        Cast(null).to<String>();
       } catch (e) {
         expect(e, isA<TypeError>());
         return;
       }
       fail('Must throw exception');
     });
-    test('Cast.asNullable<String>(\'\')', () {
+    test('Cast(\'\').toNullable<String>()', () {
       expect(empty, isNotNull);
-      final value = Cast.asNullable<String>(empty);
+      final value = Cast(empty).toNullable<String>();
       expect(value, isNotNull);
       expect(value, equals(empty));
     });
-    test('Cast.asNullable<String>(\'$sample\')', () {
+    test('Cast(\'$sample\').toNullable<String>()', () {
       expect(sample, isNotNull);
-      final value = Cast.asNullable<String>(sample);
+      final value = Cast(sample).toNullable<String>();
       expect(value, isNotNull);
       expect(value, equals(sample));
       expect(value, isNot(empty));
     });
-    test('Cast.asNullable<String>($map)', () {
+    test('Cast($map).toNullable<String>()', () {
       expect(map, isNotNull);
       expect(map, isMap);
-      final value = Cast.asNullable<String>(map);
+      final value = Cast(map).toNullable<String>();
       expect(value, isNull);
     });
-    test('Cast.asNullable<Map>($map)', () {
+    test('Cast($map).toNullable<Map>()', () {
       expect(map, isNotNull);
       expect(map, isMap);
-      final value = Cast.asNullable<Map>(map);
+      final value = Cast(map).toNullable<Map>();
       expect(value, isNotNull);
       expect(value, isMap);
       expect(value, equals(map));
     });
-    test('Cast.asNullable<Map<int, String>>($map)', () {
+    test('Cast($map).toNullable<Map<int, String>>()', () {
       expect(map, isNotNull);
       expect(map, isMap);
-      final value = Cast.asNullable<Map<int, String>>(map);
+      final value = Cast(map).toNullable<Map<int, String>>();
       expect(value, isNull);
     });
-    test('Cast.asNullable<String>(null)', () {
-      final value = Cast.asNullable<String>(null);
+    test('Cast(null).toNullable<String>()', () {
+      final value = Cast(null).toNullable<String>();
       expect(value, isNull);
     });
   });
